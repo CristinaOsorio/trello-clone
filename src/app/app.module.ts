6c92df6a82eee1a1748dda3df75d533af66da4ed
CristinaOsorio <macry_08@hotmail.com>
2024-08-08 21:39:39 -0600

    feat: generate boards component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BtnComponent } from './components/btn/btn.component';
+import { BoardsComponent } from './pages/boards/boards.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    BtnComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, BtnComponent, BoardsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
