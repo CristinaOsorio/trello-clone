c52d0b8b1f2bd33403a80ac6b62cf67c460d91cd
CristinaOsorio <macry_08@hotmail.com>
2024-08-08 23:06:51 -0600

    feat: add notification and info buttons

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   isOpen = false;
+
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
 
   constructor() {}
 
