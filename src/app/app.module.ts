6ab186faaa38a9822fe8a954118029bb5f968ad3
CristinaOsorio <macry_08@hotmail.com>
2024-08-08 21:40:10 -0600

    feat: generate navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BtnComponent } from './components/btn/btn.component';
 import { BoardsComponent } from './pages/boards/boards.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, BtnComponent, BoardsComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    BtnComponent,
+    BoardsComponent,
+    NavbarComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
