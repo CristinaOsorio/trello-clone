50f8595a9b7d669c4cca2f82f4f482f9c00a3eed
CristinaOsorio <macry_08@hotmail.com>
2024-08-08 19:47:42 -0600

    feat: create reusable button component with dynamic type and content

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  templateUrl: './btn.component.html',
+})
+export class BtnComponent implements OnInit {
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
